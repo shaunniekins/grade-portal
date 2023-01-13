693c690732ad5f55ff3e7146ee7722a4a94ea827
Shaun Niel Ochavo <shaunniel02@gmail.com>
2023-01-14 06:24:20 +0800

    Change layout of footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,14 @@
 import Navbar from "./Navbar";
-// import { useFlex } from "@chakra-ui/core";
 import Footer from "./Footer";
 import { Flex, Box } from "@chakra-ui/react";
 
 const Layout = ({ children }) => {
-  // const { flex } = useFlex();
   return (
     <Flex flexDirection="column" minH={"100vh"} w="100%">
       <Navbar />
       <Flex flexDirection="column" mb={20}>
         {children}
       </Flex>
-      {/* <Box {...flex}>{children}</Box> */}
       <Box position="absolute" bottom="0" w="100%">
         <Footer />
       </Box>
