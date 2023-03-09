faedcfdbe82a4329e5d3902b98da6e2a10ba2c2c
Shaun Niel Ochavo <shaunniel02@gmail.com>
2023-03-10 06:58:02 +0800

    Create another instance of profile card

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,4 +1,13 @@
-import { Box, Flex, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const ProfileCard = ({
   nameDisplay,
@@ -45,3 +54,84 @@ const ProfileCard = ({
 };
 
 export default ProfileCard;
+
+export const ProfileCard2 = ({
+  nameDisplay,
+  schoolYearDisplay,
+  sheetNameDisplay,
+  lrnDisplay,
+  gradeDisplay,
+  selectedSchoolOption,
+  selectedQuarterOption,
+  selectedSubjectOption,
+}) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const handleClick = () => {
+    setShowMore(!showMore);
+  };
+
+  // Array of texts
+  const texts = [
+    `Name: ${nameDisplay}`,
+    `LRN: ${lrnDisplay}`,
+    `School Year: ${schoolYearDisplay}`,
+    <Box h="1rem" />,
+    <Divider orientation="horizontal" />,
+    `Grade: ${gradeDisplay}`,
+    `Section: ${sheetNameDisplay}`,
+    `School: ${selectedSchoolOption.toUpperCase()}`,
+    `Quarter: ${
+      selectedQuarterOption.charAt(0).toUpperCase() +
+      selectedQuarterOption.slice(1)
+    } Quarter`,
+    `Subject: ${selectedSubjectOption}`,
+  ];
+
+  return (
+    <Box
+      bg="white"
+      p={3}
+      rounded="xl"
+      // boxShadow="xl"
+      w={{ base: 200, md: 400 }}
+      border={"4px"}
+      borderColor={"blue.600"}
+      overflow={showMore ? "visible" : "hidden"}
+      position={showMore ? "absolute" : "static"}
+      zIndex={showMore ? "1" : "0"}>
+      <Flex
+        direction={"column"}
+        align={"center"}
+        mb={{ base: "unset", md: "1rem" }}>
+        <Image
+          borderRadius="full"
+          boxSize={{ base: "60px", md: "90px" }}
+          objectFit="cover"
+          src="https://www.ccair.org/wp-content/uploads/2015/04/wallpaper-for-facebook-profile-photo-e1440624505574.jpg"
+          alt="Dan Abramov"
+          fallbackSrc="https://via.placeholder.com/150"
+        />
+      </Flex>
+      <VStack spacing={2} fontSize={{ base: "sm", md: "lg" }}>
+        <Flex direction={"column"} align="flex-start">
+          {texts.slice(0, 2).map((text) => (
+            <Text key={text}>{text}</Text>
+          ))}
+          {showMore ? (
+            texts.slice(2).map((text) => <Text key={text}>{text}</Text>)
+          ) : (
+            <></>
+          )}
+        </Flex>
+        <Text
+          as="kbd"
+          fontSize={{ base: "xs", md: "sm" }}
+          cursor={"pointer"}
+          onClick={handleClick}>
+          {showMore ? "Less ↑" : `More ↓`}
+        </Text>
+      </VStack>
+    </Box>
+  );
+};
